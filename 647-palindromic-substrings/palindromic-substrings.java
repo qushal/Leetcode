cc94b316097d82eab40f6aa97331ca49ebaa1e34
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-07-26 22:27:38 +0530

    Time: 5 ms (96.96%) | Memory: 41.5 MB (85.04%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,17 +1,18 @@
 class Solution {
+    int count = 0;
     public int countSubstrings(String s) {
         int n = s.length();
-        int count = 0;
-        boolean dp[][] = new boolean[n][n];
         for(int i=0; i<n; i++){
-            for(int j=i; j>=0; j--){
-                if(i==j) dp[i][j] = true;
-                else if(i == j+1) dp[i][j] = (s.charAt(i) == s.charAt(j));
-                else dp[i][j] = (s.charAt(i) == s.charAt(j)) && dp[i-1][j+1];
-
-                if(dp[i][j] == true) count++;
-            }
-        }
+            helper(s, i, i+1);
+            helper(s, i, i);
+        } 
         return count;
+    }
+    public void helper(String s, int left, int right){
+        while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
+            left--;
+            right++;
+        }
     }
 }
