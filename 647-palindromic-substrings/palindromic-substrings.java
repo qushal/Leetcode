f8cc935cf31f40575b8085409456f2a26c7dee04
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-07-26 22:21:16 +0530

    Time: 18 ms (30.19%) | Memory: 45.2 MB (28.58%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,21 +1,17 @@
 class Solution {
     public int countSubstrings(String s) {
-        if(s.length() < 2) return s.length();
-        int count = s.length();
-        for(int i=1; i<s.length(); i++){
-            for(int j=0; j<i; j++){
-                if(isPal(s.substring(j, i+1))) count++;
+        int n = s.length();
+        int count = 0;
+        boolean dp[][] = new boolean[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=i; j>=0; j--){
+                if(i==j) dp[i][j] = true;
+                else if(i == j+1) dp[i][j] = (s.charAt(i) == s.charAt(j));
+                else dp[i][j] = (s.charAt(i) == s.charAt(j)) && dp[i-1][j+1];
+
+                if(dp[i][j] == true) count++;
             }
         }
         return count;
-    }
-
-    public boolean isPal(String s){
-        int i=0, j=s.length()-1;
-        while(i<j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++; j--;
-        }
-        return true;
     }
 }
