2575f1c69ea0ff255769b7a60dc0cfc3da67d102
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-08-02 21:41:26 +0530

    Time: 574 ms (50.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,1 @@
+select round(count(player_id)/(select count(distinct player_id) from Activity), 2) as fraction from Activity where (player_id, Date_sub(event_date, interval 1 Day)) in (select distinct player_id, min(event_date) from Activity group by player_id)
