ab159e0c7fe92ae88810caaca818ea649f79456b
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-08-07 19:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (5.55%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,12 @@
+import numpy as np 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = np.array(nums)
         seen = {}
-        for i, num in enumerate(nums):
-            if(target - num ) in seen:
+
+        for i, num in enumerate(arr):
+            if (target - num) in seen:
                 return [i, seen[target-num]]
             seen[num] = i
-        
         
