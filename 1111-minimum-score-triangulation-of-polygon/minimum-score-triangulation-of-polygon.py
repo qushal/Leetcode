674b3864b72adcf0cad22386c4b7f259cd909f03
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-09-30 18:32:02 +0530

    Time: 63 ms (15.64%) | Memory: 18.3 MB (37.71%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
@@ -1,16 +1,21 @@
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
-        @lru_cache(None)
+        n = len(values)
+        dp = [[-1] * n for _ in range(n)]
+        
         def solve(i, j):
             if j - i < 2:
                 return 0
+
+            if dp[i][j] != -1:
+                return dp[i][j]
 
             result = sys.maxsize
             for k in range(i+1, j):
                 ans = solve(i,k) + values[i]*values[j]*values[k] + solve(k, j)
                 result = min(result, ans)
 
+            dp[i][j] = result
             return result
 
-        n = len(values)
         return solve(0, n-1)
