ec38cd74a76f30d65229baa42c616253ab22fcac
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-11-24 00:45:54 +0530

    Time: 247 ms (10.35%) | Memory: 48.1 MB (7.99%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -1,33 +1,18 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        remain1 = []
-        remain2 = []
-        total = 0
-        for num in nums:
-            total += num
-            if num % 3 == 1:
-                remain1.append(num)
-            if num % 3 == 2:
-                remain2.append(num)
+        n = len(nums)
+        dp = [[None] * 3 for _ in range(n+1)]
         
-        if total % 3 == 0:
-            return total
-        remain1.sort()
-        remain2.sort()
-
-        remainder = total % 3
-        result = 0 
-
-        if remainder == 1:
-            remove1 = remain1[0] if len(remain1) > 0 else float('inf')
-            remove2 = remain2[0] + remain2[1] if len(remain2) > 1 else float('inf')
+        def solve(i, remain):
+            if i >= n:
+                return 0 if remain == 0 else float('-inf')
             
-            result = max(result, total - min(remove1, remove2))
-        
-        else:
-            remove1 = remain2[0] if len(remain2) > 0 else float('inf')
-            remove2 = remain1[0] + remain1[1] if len(remain1) > 1 else float('inf')
+            if dp[i][remain] is not None:
+                return dp[i][remain]   
 
-            result = max(result, total - min(remove1, remove2))
+            take = nums[i] + solve(i+1, (remain + nums[i]) % 3)
+            skip = solve(i+1, remain)
+            dp[i][remain] = max(take, skip)
+            return dp[i][remain]
         
-        return result 
+        return solve(0, 0)
